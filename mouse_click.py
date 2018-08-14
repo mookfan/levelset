986e43208ffcb677accc24bcc2a794a66792b9b0
mookfan <mookfan@gmail.com>
2018-08-14 15:30:54 +0700

    first commit

---
+++ mouse_click.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage import color, io
+from matplotlib.widgets  import RectangleSelector
+from scipy import ndimage
+import matplotlib.patches as patches
+import cv2
+
+x, y = 0, 0 
+width, height = 0, 0
+
+
+
+def line_select_callback(eclick, erelease):
+    global x, y, width, height
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+
+    rect = patches.Rectangle( (min(x1,x2),min(y1,y2)), np.abs(x1-x2), np.abs(y1-y2) )
+    print rect
+    x, y = min(x1,x2),min(y1,y2)
+    width, height = np.abs(x1-x2), np.abs(y1-y2)
+    ax.add_patch(rect)
+
+if __name__ == "__main__":
+
+    img = cv2.imread('Feature.png', 0)
+    fig, ax = plt.subplots()
+    line = ax.imshow(img, cmap = 'gray')
+
+    rows, cols = img.shape
+    im = np.zeros((rows, cols), np.uint8)
+    rs = RectangleSelector(ax, line_select_callback,
+                        drawtype='box', useblit=False, button=[1], 
+                        minspanx=5, minspany=5, spancoords='pixels', 
+                        interactive=True)
+    plt.show()
+    # print x, y, width, height
+    x, y, width, height = int(x), int(y), int(width), int(height)
+    crop = img[y:y+height,x:x+width]
+    mean = np.mean(crop)
+    variance = ndimage.variance(crop)
+    print "mean: ", mean
+    print "variance", variance
+    # print "cov: ", np.cov(crop)
+    cv2.imshow("mask", crop)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
