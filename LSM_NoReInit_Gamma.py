78b2973503cdcdbf65652dbe895c1bb8e73959f0
mookfan <mookfan@gmail.com>
2018-09-14 10:07:41 +0700

    Updated version (for compare speed w/ LSM_NoReInit_Tensor)

--- LSM_NoReInit_Gamma.py
+++ LSM_NoReInit_Gamma.py
@@ -12,7 +12,6 @@ def divergence(fx, fy):
     f = fxx + fyy
     return f
 
-
 def deltafunction(epsilon, phi):
     k = (1.0 / (2.0 * epsilon)) * (1 + np.cos((np.pi * phi) / epsilon))
     delta = (0.0 * (np.abs(phi) > epsilon)) + (k * (np.abs(phi) <= epsilon))
@@ -45,6 +44,7 @@ def energy(img, phi, epsilon, Ix, Iy):
 
     """EXTERNAL___ENERGY"""
     delta = deltafunction(epsilon, phi)
+    # print("delta", delta.max(), delta.min())
     # !!!!! g !!!!!
     # print b
     # print "+++"
@@ -82,12 +82,18 @@ def energy(img, phi, epsilon, Ix, Iy):
     # print(gamma_energy.max(), gamma_energy.min())
 
     """Because intensity of this image has only two values so we can map from it's intensity to it's probability"""
-    gp = (1*(img == 255))+(0.00001*(img == 0))
-    gn = (1 * (img == 0)) + (0.00001 * (img == 255))
+    # gp = (1*(img == 255))+(0.00001*(img == 0))
+    # gn = (1 * (img == 0)) + (0.00001 * (img == 255))
+
+    gp = (1.0 * (img >= 100)) + (0.00001 * (img < 100))
+    gn = (1.0 * (img < 100)) + (0.00001 * (img >= 100))
+    # cv2.imshow("gp", gp)
+    # cv2.imshow("gn", gn)
+    # cv2.waitKey(1)
     gamma_energy = (np.log(gn / gp)) * delta
 
-
-    return inner, exter_length, exter_area, gamma_energy
+    debug = div
+    return inner, exter_length, exter_area, gamma_energy, debug
 
 
 def process(img, img_gray):
@@ -98,7 +104,10 @@ def process(img, img_gray):
     # phi[101:299, 101:299] = init_phi
     """some part of pipeline """
     phi[200:500, 300:600] = init_phi
+    # phi[100:150, 100:250] = init_phi
     # phi[201:499, 301:599] = init_phi
+
+    # phi[50:500, 300:600] = init_phi
     """ whole pipeline """
     # phi[200:600, 300:720] = phi[200:600, 300:720]+init_phi
     # phi[201:599, 301:719] = init_phi
@@ -108,6 +117,7 @@ def process(img, img_gray):
     mew = 0.12  # 0.12
     lamda = 50.0  # 5.0
     v = -5.0  # -5.0 #initial inside: -
+    beta = 1.0
     epsilon = 1.0  # 1.5
     time = 0.5  # 0.5
     sigma = 1.0  # 1.0
@@ -115,6 +125,7 @@ def process(img, img_gray):
     coeff1 = time * mew
     coeff2 = time * lamda
     coeff3 = time * v
+    coeff4 = time * beta
     print("coeff1: %.2f, coeff2: %.2f, coeff3: %.2f" % (coeff1, coeff2, coeff3))
 
     # plt.imshow(img_gray)
@@ -156,11 +167,19 @@ def process(img, img_gray):
     for i in range(0, iter):
         print("times#: ", i)
         print("phi_max: %f, phi_min: %f" % (phi.max(), phi.min()))
-        internal, external_length, external_area, gamma_energy = energy(img_gray, phi, epsilon, Ix, Iy)
+        internal, external_length, external_area, gamma_energy,  debug = energy(img_gray, phi, epsilon, Ix, Iy)
+        plt.imshow(debug)
+        plt.show()
         # print("part1: %.2f, part2: %.2f, part3: %.2f, part4: %.2f" % (internal, external_length, external_area, gamma_energy))
         """-gamma_energy because pipe area returns negative energy BUT phi is determined positive inside the pipe. """
-        phi = phi + (coeff1 * internal) + (coeff2 * external_length) + (coeff3 * external_area) - gamma_energy
+        phi = phi + (coeff1 * internal) + (coeff2 * external_length) + (coeff3 * external_area) - (coeff4 * gamma_energy)
         phi = phi.astype('float32')
+        print("a: %.2f, b: %.2f, c: %.2f, d: %.2f" % ((coeff1 * internal).max(), (coeff2 * external_length).max(), (coeff3 * external_area).max(), (beta * gamma_energy).max()))
+        # if i % 1 == 0:
+        #     plt.imshow(internal)
+        #     plt.show()
+        #     plt.pause(0.05)
+        #     plt.clf()
         if i % 10 == 0:
             plt.imshow(phi)
             CS = plt.contour(phi, 0, colors='r', linewidths=2)
@@ -184,7 +203,9 @@ def process(img, img_gray):
 
 
 if __name__ == "__main__":
+    # img = cv2.imread('original.png')
     img = cv2.imread('Feature.png')
+    print(img.shape)
     # img = cv2.imread('image_RTheta.jpg')
     # img = cv2.imread('fig2.jpg')
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
