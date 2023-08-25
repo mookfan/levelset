2aa455d53a5b877e6c7aba4ed9d9264724d41287
mook <mookfan@gmail.com>
2023-08-25 13:10:24 +0700

    ver5.1

--- open_pickle.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import pickle as pl
-import numpy as np
-
-fig_handle = pl.load(open(r"C:\Users\mook\PycharmProjects\LSM\experiment\results\skeleton_71.pickle",'rb'))
-plt.show()
